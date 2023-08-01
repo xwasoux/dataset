37ce27ed8a6db85068ea1903b6d3df81f47644af
xwasoux <oxwasouxo@gmail.com>
2023-08-01 06:22:25 +0000

    Update: Dir name & Arguments

--- CodeSearchNet/sh/astcutting.sh
+++ CodeSearchNet/sh/astcutting.sh
@@ -1,16 +1,18 @@
-CURRENT=$(cd $(dirname $0);pwd)
+CURRENT=$(pwd)
 
-mkdir ../data ../data/deploy ../data/astcutting
-cd ../data/deploy
-# gdown 1rd2Tc6oUWBo7JouwexW3ksQ0PaOhUr6h
-# unzip Cleaned_CodeSearchNet.zip
-# rm Cleaned_CodeSearchNet.zip
+mkdir ../data ../data/workbench ../data/astcutting
+cd ../data/workbench
+gdown 1rd2Tc6oUWBo7JouwexW3ksQ0PaOhUr6h
+unzip Cleaned_CodeSearchNet.zip
+rm Cleaned_CodeSearchNet.zip
 
 cd $CURRENT
 
 lang=python
-basedir=../data/deploy/CodeSearchNet
+source_base_dir=../data/workbench/CodeSearchNet
+target_base_dir=../data/astcutting
 
 python3 ../src/astcutting.py \
-    --language $lang \
-    --basedir $basedir
+    --lang $lang \
+    --source_base_dir $source_base_dir \
+    --target_base_dir $target_base_dir
