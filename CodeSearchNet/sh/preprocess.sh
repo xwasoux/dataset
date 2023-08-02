afb3a9960a20eff0f4cefacffcab5659ec6e3a16
xwasoux <oxwasouxo@gmail.com>
2023-08-02 21:07:34 +0000

    Add: Addressing the token num limits of embeddings

---
+++ CodeSearchNet/sh/preprocess.sh
@@ -0,0 +1,22 @@
+CURRENT=$(pwd)
+
+mkdir ../data ../data/workbench ../data/workbench/Extracted
+mkdir ../data/CuttingAst
+# cd ../data/workbench
+# gdown 1rd2Tc6oUWBo7JouwexW3ksQ0PaOhUr6h
+# unzip Cleaned_CodeSearchNet.zip
+# rm Cleaned_CodeSearchNet.zip
+
+# cd $CURRENT
+
+lang=python
+source_base_dir=../data/workbench/CodeSearchNet
+target_base_dir=../data/workbench/Extracted
+pretrained_model=microsoft/codebert-base
+
+python3 ../src/preprocess.py \
+    --lang $lang \
+    --source_base_dir $source_base_dir \
+    --target_base_dir $target_base_dir \
+    --pretrained_model $pretrained_model
+
