6d663ce7e0ce722ebabd6e6344acc706309757f0
xwasoux <oxwasouxo@gmail.com>
2023-09-29 17:50:12 +0000

    refactor: remove unnecessary codes

--- dataset/diff/sh/run.sh
+++
@@ -1,9 +0,0 @@
-github_link=##repository
-
-cd ..
-mkdir diff-commit workbench
-python3 datacollation.py \
-    --repo_link $github_link \
-    --output_dir diff/##name
-
-cd sh/
--- dataset/diff/sh/zip.sh
+++
@@ -1,9 +0,0 @@
-cd ..
-mkdir zip
-
-target_path=diff-commit/wakame-tech/12_step_emb_os
-zip_name=wakame-tech/12_step_emb_os.zip
-
-zip -r zip/zipjsonl $target_path
-
-cd sh/
